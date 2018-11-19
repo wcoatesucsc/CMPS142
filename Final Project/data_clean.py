d30cdbb45fccd67924c1ef40013f61592d4c38c7
wcoatesucsc <wcoates@ucsc.edu>
2018-11-18 17:34:44 -0800

    data_clean.py now takes train.csv in, prints all its rows with cleaned phrases to clean_train.csv

--- Final Project/data_clean.py
+++ Final Project/data_clean.py
@@ -18,36 +18,68 @@
 #
 
 
+# Machine Learning Libraries Imports
+import nltk
+from nltk.corpus import stopwords
+nltk.download('wordnet')
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
 
+import csv
+import string
 
-
-
-
-
-def main():
-	print("Cleaning data! :)")
-
-	# load the document
-	filename = 'train.csv'
-
-
-	# make all text lowercase
-	textLower = text.lower()
-	
-	# split into tokens by white space
-	tokens = textLower.split()
-
-	#
-	# Start cleaning and removing tokens
-	#
-
+def clean_phrase(phrase):
+	# convert phrase to lowercase
+	phraseLower = phrase.lower()
+	# tokenize the phrase (now it's a list of strings)
+	tokens = phraseLower.split()
 	# remove punctuation from each token
 	table = str.maketrans('', '', string.punctuation)
 	tokens = [w.translate(table) for w in tokens]
 
+	# remove other non-alphabetic tokens
+	tokens = [word for word in tokens if word.isalpha()]
+	# filter out stop words
+	stop_words = set(stopwords.words('english'))
+	# filter out short tokens
+	tokens = [word for word in tokens if len(word) > 1]
+	# 
+	# OUR ADDITIONS TO TUTORIAL CLEANING METHODS
+	# 
+	# lemmatization:
+	tokens = [lemmatizer.lemmatize(word) for word in tokens]
+			
+	# converting list of tokens back to a string
+	separator = " "
+	cleanedPhrase = separator.join(tokens)
+	return cleanedPhrase
+			
 
 
-	print(tokens)
+def main():
+	print("Cleaning data! :)")
+	# simultaneously read from train.csv and write to clean_train.csv
+	with open('train.csv', 'r', newline='') as incsvfile:
+		with open('clean_train.csv', 'w', newline='') as outcsvfile:
+			csv_reader = csv.reader(incsvfile, delimiter=",")
+			
+			csv_writer = csv.writer(outcsvfile, delimiter=",")
+			# write header row
+			# NOTE! WHEN RUNNING ON TEST DATA EXCLUDE SENTIMENT!
+			#csv_writer.writerow(['PhraseId', 'SentenceId', 'Phrase', 'Sentiment']) 
+
+			rownum = 0	
+			for row in csv_reader:
+				rownum += 1
+				print("row: " + str(rownum))
+				# grab phrase and apply cleaning procedures to it
+				phrase = row[2]
+				cleanedPhrase = clean_phrase(phrase)
+				# write the same row (except with a clean phrase) to the output file
+				#NOTE! WHEN RUNNING ON TEST DATA EXCLUDE ROW[3]!!
+				csv_writer.writerow([row[0], row[1], cleanedPhrase, row[3]])
+	incsvfile.close()
+	outcsvfile.close()	
 
 
 main()
